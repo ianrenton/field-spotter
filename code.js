8ab04d168bf629de670e35f6d819732aaf318733
Ian Renton <ian@ianrenton.com>
2024-09-20 08:35:25 +0100

    Improve contrast of icons and text against band-coloured backgrounds

--- code.js
+++ code.js
@@ -10,22 +10,22 @@ const WWFF_SPOTS_URL = "https://www.cqgma.org/api/spots/wwff/";
 const BASEMAP_NAME = "Esri.NatGeoWorldMap";
 const BASEMAP_OPACITY = 0.5;
 const BANDS = [
-{ name: "160m", startFreq: 1.8, stopFreq: 2.0, color: "#7cfc00" },
-{ name: "80m", startFreq: 3.5, stopFreq: 4.0, color: "#e550e5" },
-{ name: "60m", startFreq: 5.25, stopFreq: 5.41, color: "#00008b" },
-{ name: "40m", startFreq: 7.0, stopFreq: 7.3, color: "#5959ff" },
-{ name: "30m", startFreq: 10.1, stopFreq: 10.15, color: "#62d962" },
-{ name: "20m", startFreq: 14.0, stopFreq: 14.35, color: "#f2c40c" },
-{ name: "17m", startFreq: 18.068, stopFreq: 18.168, color: "#f2f261" },
-{ name: "15m", startFreq: 21.0, stopFreq: 21.45, color: "#cca166" },
-{ name: "12m", startFreq: 24.89, stopFreq: 24.99, color: "#b22222" },
-{ name: "10m", startFreq: 28.0, stopFreq: 29.7, color: "#ff69b4" },
-{ name: "6m", startFreq: 50.0, stopFreq: 54.0, color: "#FF0000" },
-{ name: "4m", startFreq: 70.0, stopFreq: 70.5, color: "#cc0044" },
-{ name: "2m", startFreq: 144.0, stopFreq: 148.0, color: "#FF1493" },
-{ name: "70cm", startFreq: 420.0, stopFreq: 450.0, color: "#999900" },
-{ name: "23cm", startFreq: 1240.0, stopFreq: 1325.0, color: "#5AB8C7" },
-{ name: "13cm", startFreq: 2300.0, stopFreq: 2450.0, color: "#FF7F50" }];
+{ name: "160m", startFreq: 1.8, stopFreq: 2.0, color: "#7cfc00", contrastColor: "black" },
+{ name: "80m", startFreq: 3.5, stopFreq: 4.0, color: "#e550e5", contrastColor: "black" },
+{ name: "60m", startFreq: 5.25, stopFreq: 5.41, color: "#00008b", contrastColor: "white" },
+{ name: "40m", startFreq: 7.0, stopFreq: 7.3, color: "#5959ff", contrastColor: "white" },
+{ name: "30m", startFreq: 10.1, stopFreq: 10.15, color: "#62d962", contrastColor: "black" },
+{ name: "20m", startFreq: 14.0, stopFreq: 14.35, color: "#f2c40c", contrastColor: "black" },
+{ name: "17m", startFreq: 18.068, stopFreq: 18.168, color: "#f2f261", contrastColor: "black" },
+{ name: "15m", startFreq: 21.0, stopFreq: 21.45, color: "#cca166", contrastColor: "black" },
+{ name: "12m", startFreq: 24.89, stopFreq: 24.99, color: "#b22222", contrastColor: "white" },
+{ name: "10m", startFreq: 28.0, stopFreq: 29.7, color: "#ff69b4", contrastColor: "black" },
+{ name: "6m", startFreq: 50.0, stopFreq: 54.0, color: "#FF0000", contrastColor: "white" },
+{ name: "4m", startFreq: 70.0, stopFreq: 70.5, color: "#cc0044", contrastColor: "white" },
+{ name: "2m", startFreq: 144.0, stopFreq: 148.0, color: "#FF1493", contrastColor: "black" },
+{ name: "70cm", startFreq: 420.0, stopFreq: 450.0, color: "#999900", contrastColor: "white" },
+{ name: "23cm", startFreq: 1240.0, stopFreq: 1325.0, color: "#5AB8C7", contrastColor: "black" },
+{ name: "13cm", startFreq: 2300.0, stopFreq: 2450.0, color: "#FF7F50", contrastColor: "black" }];
 
 
 /////////////////////////////
@@ -405,7 +405,7 @@ async function recalculateBandsPanelContent() {
   bandToSpots.forEach(function (spotList, bandName, map) {
     band = BANDS.filter(function (b) { return b.name == bandName; })[0];
     html += "<div class='bandCol' style='width:" + columnWidthPercent + "%'>";
-    html += "<div class='bandColHeader' style='background-color:" + band.color + "'>" + band.name + "</div>";
+    html += "<div class='bandColHeader' style='background-color:" + band.color + "; color:" + band.contrastColor + "'>" + band.name + "</div>";
     html += "<div class='bandColMiddle'>";
 
     var freqStep = (band.stopFreq - band.startFreq) / 40.0;
@@ -601,7 +601,7 @@ function freqToBand(f) {
   return "Unknown";
 }
 
-// Convert a frequency to a colour to use on marker icons.
+// Convert a frequency to a colour to use on markers or frequency band headers.
 function freqToColor(f) {
   for (band of BANDS) {
     if (f >= band.startFreq && f <= band.stopFreq) {
@@ -609,6 +609,16 @@ function freqToColor(f) {
     }
   }
   return "gray";
+}
+
+// Convert a frequency to a colour to use on marker icons or text that should contrast with the normal band colour.
+function freqToContrastColor(f) {
+  for (band of BANDS) {
+    if (f >= band.startFreq && f <= band.stopFreq) {
+      return band.contrastColor
+    }
+  }
+  return "black";
 }
 
 // Get an icon for a spot, based on its band, using PSK Reporter colours, its program etc.
@@ -621,6 +631,7 @@ function getIcon(s) {
   }
   return L.ExtraMarkers.icon({
     icon: icon,
+    iconColor: preQSYStatusShouldShowGrey(s.preqsy) ? "black" : freqToContrastColor(s.freq),
     markerColor: preQSYStatusShouldShowGrey(s.preqsy) ? "gray" : freqToColor(s.freq),
     shape: 'circle',
     prefix: 'fa',
