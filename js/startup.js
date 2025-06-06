acbba567b75d60dd1f159c473e316f8ff8c9a13e
Ian Renton <ian@ianrenton.com>
2025-06-06 07:46:40 +0100

    Remove test code

--- js/startup.js
+++ js/startup.js
@@ -17,39 +17,3 @@ setInterval(checkForUpdate, 1000);
 setInterval(function () {
     terminator.setTime()
 }, 300);
-
-
-
-// todo remove
-let test = JSON.parse("[\n" +
-    "  {\n" +
-    "    \"call\": \"M1SDH/P\",\n" +
-    "    \"comment\": \"B/G-2114,2115 POTA GB-0051 WAB SU03\",\n" +
-    "    \"freq\": 7.168,\n" +
-    "    \"mode\": \"SSB\",\n" +
-    "    \"references\": [\n" +
-    "      {\n" +
-    "        \"lat\": 51.112643,\n" +
-    "        \"locator\": \"IO91AC\",\n" +
-    "        \"long\": -1.967547,\n" +
-    "        \"name\": \"WW2 Bunker No.1 (Grovely Woods)\",\n" +
-    "        \"reference\": \"B/G-2114\",\n" +
-    "        \"scheme\": \"UKBOTA\",\n" +
-    "        \"type\": \"WW2 Bunker\"\n" +
-    "      },\n" +
-    "      {\n" +
-    "        \"lat\": 51.108519,\n" +
-    "        \"locator\": \"IO91AC\",\n" +
-    "        \"long\": -1.944051,\n" +
-    "        \"name\": \"WW2 Bunker No.2 (Grovely Woods)\",\n" +
-    "        \"reference\": \"B/G-2115\",\n" +
-    "        \"scheme\": \"UKBOTA\",\n" +
-    "        \"type\": \"WW2 Bunker\"\n" +
-    "      }\n" +
-    "    ],\n" +
-    "    \"spotter\": \"M0ICR\",\n" +
-    "    \"time\": \"2025-04-18T08:14:10.313Z\",\n" +
-    "    \"type\": \"Live\"\n" +
-    "  }\n" +
-    "]");
-handleWWBOTAData(test);
