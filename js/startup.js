f1952b5e61915c97475e1dc1efa1c4a4997f9868
Ian Renton <ian@ianrenton.com>
2025-06-08 14:25:34 +0100

    Add small startup delay before querying API to allow libraries to load.

--- js/startup.js
+++ js/startup.js
@@ -8,8 +8,9 @@ setUpMap();
 loadLocalStorage();
 // Request geolocation
 requestGeolocation();
-// Load data for the first time
-fetchData();
+// Load data for the first time. We add a half second delay here to ensure libraries have fully
+// loaded before we start fetching data.
+setTimeout(fetchData, 500);
 // Every second, check if we need to update data based on the user's configured update interval,
 // and update other UI elements regarding data age.
 setInterval(checkForUpdate, 1000);
